efbe9c7a3ea71eb8c0685026cd50caff0e50e8ec
Excalibur.H <2304200957@qq.com>
2025-01-09 10:58:35 +0800

    - alic

---
+++ jushan-mq/src/main/java/com/jushan/App.java
@@ -0,0 +1,58 @@
+package com.jushan;
+
+
+import org.apache.rocketmq.client.apis.ClientConfiguration;
+import org.apache.rocketmq.client.apis.ClientConfigurationBuilder;
+import org.apache.rocketmq.client.apis.ClientException;
+import org.apache.rocketmq.client.apis.ClientServiceProvider;
+import org.apache.rocketmq.client.apis.message.Message;
+import org.apache.rocketmq.client.apis.producer.Producer;
+import org.apache.rocketmq.client.apis.producer.SendReceipt;
+import org.apache.rocketmq.shaded.org.slf4j.Logger;
+import org.apache.rocketmq.shaded.org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) throws ClientException, IOException
+    {
+        // 接入点地址，需要设置成Proxy的地址和端口列表，一般是xxx:8080;xxx:8081
+        // 此处为示例，实际使用时请替换为真实的 Proxy 地址和端口
+        String endpoint = "101.126.19.112:8081";
+        // 消息发送的目标Topic名称，需要提前创建。
+        String topic = "TestTopic";
+        ClientServiceProvider provider = ClientServiceProvider.loadService();
+        ClientConfigurationBuilder builder = ClientConfiguration.newBuilder().setEndpoints(endpoint);
+        ClientConfiguration configuration = builder.build();
+        // 初始化Producer时需要设置通信配置以及预绑定的Topic。
+        Producer producer = provider.newProducerBuilder()
+                .setTopics(topic)
+                .setClientConfiguration(configuration)
+                .build();
+        // 普通消息发送。
+        Message message = provider.newMessageBuilder()
+                .setTopic(topic)
+                // 设置消息索引键，可根据关键字精确查找某条消息。
+                .setKeys("messageKey")
+                // 设置消息Tag，用于消费端根据指定Tag过滤消息。
+                .setTag("messageTag")
+                // 消息体。
+                .setBody("messageBody123123".getBytes())
+                .build();
+        try {
+            // 发送消息，需要关注发送结果，并捕获失败等异常。
+            SendReceipt sendReceipt = producer.send(message);
+            logger.info("Send message successfully, messageId={}", sendReceipt.getMessageId());
+        } catch (ClientException e) {
+            logger.error("Failed to send message", e);
+        }
+//         producer.close();
+    }
+}
